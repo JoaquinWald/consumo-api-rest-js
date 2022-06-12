5afed9525900cabbc31e94a7090c85764dc57d12
Joaquin Grunwald <joaquingrunwald@gmail.com>
2022-06-12 18:11:07 -0300

    Modified html and js files

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const API_URL =
-  "https://api.thecatapi.com/v1/images/search?limit=3&api_key=4f4a66e5-46aa-4de3-9bef-13d690177b37";
+const API_URL_RANDOM =
+  "https://api.thecatapi.com/v1/images/search?limit=2&api_key=4f4a66e5-46aa-4de3-9bef-13d690177b37";
+const API_URL_FAVORITES =
+  "https://api.thecatapi.com/v1/favourites?limit=2&api_key=4f4a66e5-46aa-4de3-9bef-13d690177b37";
+
+const spanError = document.querySelector("#error");
 
 //Promise
 /*fetch(URL)
@@ -11,25 +15,45 @@ const API_URL =
 */
 
 //async await
-const getCat = async () => {
+const loadRandomMichis = async () => {
   try {
-    const resAPI = await fetch(API_URL);
-    const api = await resAPI.json();
+    const resAPI = await fetch(API_URL_RANDOM);
+    const data = await resAPI.json();
+    console.log("Random");
+    console.log(data);
 
-    const img1 = document.getElementById("img1");
-    const img2 = document.getElementById("img2");
-    const img3 = document.getElementById("img3");
+    if (resAPI.status !== 200) {
+      spanError.innerHTML = "Hubo un error: " + resAPI.status;
+    } else {
+      const img1 = document.getElementById("img1");
+      const img2 = document.getElementById("img2");
 
-    console.log(api);
-    img1.src = api[0].url;
-    img2.src = api[1].url;
-    img3.src = api[2].url;
+      img1.src = data[0].url;
+      img2.src = data[1].url;
+    }
   } catch (error) {
     console.log(error);
   }
 };
-getCat();
+
+const loadFavoritesMichis = async () => {
+  try {
+    const resAPI = await fetch(API_URL_FAVORITES);
+    const data = await resAPI.json();
+    console.log("Favorites");
+    console.log(data);
+
+    if (resAPI.status !== 200) {
+      spanError.innerHTML = "Hubo un error: " + resAPI.status;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadRandomMichis();
+loadFavoritesMichis();
 
 const restartImage = document.querySelector("#restart__button");
 
-restartImage.addEventListener("click", getCat);
+restartImage.addEventListener("click", loadRandomMichis);
